728955eb637567761844dc763e472d5ebb61283c
Prathamesh Kumbhar <pratham11pmx@gmail.com>
2022-03-12 00:39:05 +0530

    Chap3 PSQ

---
+++ app/src/main/java/com/example/companyaboutuspageapplicationchap3psq4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.companyaboutuspageapplicationchap3psq4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
